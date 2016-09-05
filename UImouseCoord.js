9ec4612e4665da22ba669f084fb77348c13a8530
Utkarsh Bajpai <ubajpai1aa@gmail.com>
2016-09-05 19:41:13 +0530

    get the mouse Coordinate

---
+++ UImouseCoord.js
@@ -0,0 +1,11 @@
+(function($){
+	
+	$.fn.xy = function(){
+		return this.each(function(){
+			$(this).mousemove(function(event){
+				$('.result').text( "(" + event.pageX + " , "  + event.pageY + ")"  );
+			});
+		});
+	};
+	
+})(jQuery);
